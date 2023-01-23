e2544d837e10f6accd99c0f80054c7a6317ec49a
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-01-22 20:22:04 -0500

    starting hw

--- data-code/1_Plan_Data.R
+++ data-code/1_Plan_Data.R
@@ -1,6 +1,8 @@
 #########################################################################
 ## Read in enrollment data for january of each year
 #########################################################################
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, readr)
 
 for (y in 2007:2015) {
   ## Basic contract/plan information
