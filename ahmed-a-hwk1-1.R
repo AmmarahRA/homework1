29fa1e98f52e4f178df29bd442398359a2988472
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-01-23 20:38:28 -0500

    homework attempt

--- ahmed-a-hwk1-1.R
+++ ahmed-a-hwk1-1.R
@@ -5,5 +5,16 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 full.ma.data<- readRDS("data/output/full_ma_data.rds")
 #2
 unique(full.ma.data$plan_type)
+#3
+knitr::kable(full.ma.data, col.names=c("2010","2011","2012","2013","2014","2015"),
+             type="html", caption = "Plan Count by Year", booktabs = TRUE)
+#4
+full.ma.data2<- full.ma.data %>% 
+  filter(snp=='Yes'& eghp=='Yes')
 
-#plan.premiums<- readRDS(data/plan.premiums.rds)
+knitr::kable(full.ma.data2, col.names=c("2010","2011","2012","2013","2014","2015"),
+             type="html", caption = "Plan Count by Year", booktabs = TRUE)
+
+#Premium data
+plan.premiums<- readRDS("data/output/plan_premiums.rds")
+
