6335d0e3ff053dfd2e2c304e2853326103f369f8
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-01-23 22:15:53 -0500

    homework update

--- ahmed-a-hwk1-1.R
+++ ahmed-a-hwk1-1.R
@@ -10,10 +10,16 @@ knitr::kable(full.ma.data, col.names=c("2010","2011","2012","2013","2014","2015"
              type="html", caption = "Plan Count by Year", booktabs = TRUE)
 #4
 full.ma.data2<- full.ma.data %>% 
-  filter(snp=='Yes'& eghp=='Yes')
+  filter(snp=='Yes'& eghp=='Yes' & (planid < 800 | planid >= 900))
 
 knitr::kable(full.ma.data2, col.names=c("2010","2011","2012","2013","2014","2015"),
              type="html", caption = "Plan Count by Year", booktabs = TRUE)
+#5
+final.data <- readRDS("data/output/final_ma_data.rds")
+
+ggplot(data = final.data, aes(x=year, y=avg_enrollment)) +
+  geom_line() + ggtitle("Average Enrollees in Medicare Advantage") +
+  labs(x="Year", y="Average Enrollment")
 
 #Premium data
 plan.premiums<- readRDS("data/output/plan_premiums.rds")
